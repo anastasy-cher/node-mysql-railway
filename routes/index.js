10dfd7d09b1ebb6c8a382e3f5e978dd7be78c96e
anstcher <anastasiacher73@gmail.com>
2023-01-25 13:36:25 +0100

    lista para produccion

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,29 @@
+const dotenv = require('dotenv')
+dotenv.config()
 const { json } = require('express');
-var express = require('express');
+const express = require('express');
 const { pool } = require('../db.js');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+  const [rows] = await pool.promise().query("SELECT * FROM users")
+  res.json(rows)
 });
 
 router.get('/ping', async function(req, res, next) {
-  const result = await pool.promise().query('SELECT "HELLO WORLD" as RESULT')
+  const result = await pool.promise().query('SELECT * from users')
   console.log(result)
-  console.log(result[0])
-  res.json(result[0])
+  // console.log(result[0])
+  res.json(result)
   res.render("ping");
+});
+
+router.get('/create', async function(req, res, next) {
+  const result = await pool.promise().query('INSERT INTO users(NAME) VALUES ("pusoc")')
+  console.log(result)
+
+  res.send("ping");
 });
 
 module.exports = router;
